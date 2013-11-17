a54a47fa9feb867c5be330a8f208b6cc6888d3ee
Meagan Waller <meaganewaller@gmail.com>
2013-11-16 19:32:00 -0500

    Refactors test code and allows for white token creation if correct color wrong place in all 4 slots

--- src/com/mastermind/Game.java
+++ src/com/mastermind/Game.java
@@ -13,28 +13,28 @@ public class Game {
         Color firstGuessCode = code.getCode1();
         if (firstGuessCode.equals(secretCode.getCode1())) {
             numBlacks++;
-        } else if (firstGuessCode.equals(secretCode.getCode2())) {
+        } else if (firstGuessCode.equals(secretCode.getCode2()) || (firstGuessCode.equals(secretCode.getCode3()) || (firstGuessCode.equals(secretCode.getCode4())))) {
             numWhites++;
         }
 
         Color secondGuessCode = code.getCode2();
         if(secondGuessCode.equals(secretCode.getCode2())) {
             numBlacks++;
-        } else if (secondGuessCode.equals(secretCode.getCode1())) {
+        } else if (secondGuessCode.equals(secretCode.getCode1()) || (secondGuessCode.equals(secretCode.getCode3()) || (secondGuessCode.equals(secretCode.getCode4())))) {
             numWhites++;
         }
 
         Color thirdGuessCode = code.getCode3();
         if(thirdGuessCode.equals(secretCode.getCode3())) {
             numBlacks++;
-        } else if (thirdGuessCode.equals(secretCode.getCode4())) {
+        } else if (thirdGuessCode.equals(secretCode.getCode4()) || (thirdGuessCode.equals(secretCode.getCode1()) || (thirdGuessCode.equals(secretCode.getCode2())))) {
             numWhites++;
         }
 
         Color fourthGuessCode = code.getCode4();
         if(fourthGuessCode.equals(secretCode.getCode4())) {
             numBlacks++;
-        } else if (fourthGuessCode.equals(secretCode.getCode3())) {
+        } else if (fourthGuessCode.equals(secretCode.getCode3()) || (fourthGuessCode.equals(secretCode.getCode1()) || (fourthGuessCode.equals(secretCode.getCode2())))) {
             numWhites++;
         }
 
--- test/com/mastermind/GameTest.java
+++ test/com/mastermind/GameTest.java
@@ -1,15 +1,16 @@
 package com.mastermind;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 
 public class GameTest {
+    Code secretCode = new Code(Color.red, Color.green, Color.blue, Color.yellow);
+    Game game = new Game(secretCode);
 
     @Test
     public void returnsAllBlackWhenCorrectCode() {
-        Code secretCode = new Code(Color.red, Color.green, Color.blue, Color.yellow);
-        Game game = new Game(secretCode);
         Feedback feedback = game.getFeedback(new Code(Color.red, Color.green, Color.blue, Color.yellow));
         assertEquals(new Integer(4), feedback.getNumberOfBlack());
         assertEquals(new Integer(0), feedback.getNumberOfWhite());
@@ -17,8 +18,6 @@ public class GameTest {
 
     @Test
     public void return2BlacksAnd2WhitesWhen2CorrectOrder2Unordered() {
-        Code secretCode = new Code(Color.red, Color.green, Color.blue, Color.yellow);
-        Game game = new Game(secretCode);
         Feedback feedback = game.getFeedback(new Code(Color.green, Color.red, Color.blue, Color.yellow));
         assertEquals(new Integer(2), feedback.getNumberOfBlack());
         assertEquals(new Integer(2), feedback.getNumberOfWhite());
@@ -26,11 +25,16 @@ public class GameTest {
 
     @Test
     public void return2BlacksAnd2WhitesWhen2Unordered2CorrectOrder() {
-        Code secretCode = new Code(Color.red, Color.green, Color.blue, Color.yellow);
-        Game game = new Game(secretCode);
         Feedback feedback = game.getFeedback(new Code(Color.red, Color.green, Color.yellow, Color.blue));
         assertEquals(new Integer(2), feedback.getNumberOfBlack());
         assertEquals(new Integer(2), feedback.getNumberOfWhite());
-
     }
+
+    @Test
+    public void return1BlackAnd2WhiteWhen1Correct2Unordered() {
+        Feedback feedback = game.getFeedback(new Code(Color.red, Color.blue, Color.green, Color.orange));
+        assertEquals(new Integer(1), feedback.getNumberOfBlack());
+        assertEquals(new Integer(2), feedback.getNumberOfWhite());
+    }
+
 }
