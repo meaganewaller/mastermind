0f8f58199c1e86aa44bc7ea62dbc12658e302c0c
Meagan Waller <meaganewaller@gmail.com>
2013-11-18 10:00:09 -0500

    Tests to make sure that random actually generates a random color

--- test/com/mastermind/ColorTest.java
+++ test/com/mastermind/ColorTest.java
@@ -2,7 +2,11 @@ package com.mastermind;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class ColorTest {
     @Test
@@ -14,6 +18,20 @@ public class ColorTest {
         assertNotNull(Color.purple);
         assertNotNull(Color.orange);
     }
+
+    @Test
+    public void randomReturnsAColor() {
+        List<Color> colors =  new ArrayList<Color>();
+        colors.add(Color.red);
+        colors.add(Color.blue);
+        colors.add(Color.green);
+        colors.add(Color.yellow);
+        colors.add(Color.purple);
+        colors.add(Color.orange);
+        assertTrue(colors.contains(Color.random()));
+    }
+
+
 
 
 }
