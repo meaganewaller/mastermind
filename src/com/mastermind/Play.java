52d195d395d0789bf551280c8626a11531298cfa
Meagan Waller <meaganewaller@gmail.com>
2013-11-17 19:06:31 -0500

    Testing out how BufferedIn and System out work, incrementing turns

--- src/com/mastermind/Play.java
+++ src/com/mastermind/Play.java
@@ -1,18 +1,53 @@
 package com.mastermind;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Play {
-    public Code generateRandomSecretCode() {
+    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    public static int turns = 0;
+
+
+    public static void main(String[] args) throws IOException {
+        welcomeMessage();
+        promptForGuess();
+        Code secretCode = generateRandomSecretCode();
+        String guess = null;
+        while ((guess = in.readLine()) != null) {
+            turns++;
+            System.out.println("You're on turn number " + turns);
+            System.out.println(guess);
+            promptForGuess();
+
+        }
+        System.out.println("Your guess was: " + guess);
+
+    }
+
+    public static void welcomeMessage() {
+        System.out.print("Welcome To MasterMind!\n");
+        System.out.print("Guess The 4 Digit Secret Code, Colors Represented by Values 1-6\n");
+    }
+
+    public static void promptForGuess() {
+        System.out.print("What's your guess?: ");
+    }
+
+
+
+    public static Code generateRandomSecretCode() {
         Code secretCode = new Code(generateRandomColor(), generateRandomColor(), generateRandomColor(), generateRandomColor());
         return secretCode;
     }
 
-    private Color generateRandomColor() {
+    private static Color generateRandomColor() {
         int value = new Long(Math.round(Math.random() * 6)).intValue();
         Color randomColor = getColorFromInt(value);
         return randomColor;
     }
 
-    private Color getColorFromInt(int value) {
+    private static Color getColorFromInt(int value) {
         Color randomColor = null;
         switch (value) {
             case 1:
--- test/com/mastermind/PlayTest.java
+++ test/com/mastermind/PlayTest.java
@@ -3,7 +3,7 @@ package com.mastermind;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
-
+import static org.junit.Assert.assertSame;
 
 
 public class PlayTest {
@@ -12,10 +12,20 @@ public class PlayTest {
         Play play = new Play();
         Code secretCode = play.generateRandomSecretCode();
         assertNotNull(secretCode);
+    }
 
+    /*
+    @Test
+    public void startsAtZeroTurns() {
+        Play play = new Play();
+        assertSame(new Integer(0), play.turns());
 
 
     }
+    */
+
+
+
 
 
 
