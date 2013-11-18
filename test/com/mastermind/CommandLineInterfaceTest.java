c45e43e4f73aa25c4b8088627cc3584eb4b1f74a
Meagan Waller <meaganewaller@gmail.com>
2013-11-17 20:00:57 -0500

    converts users guess to all lowercase

--- test/com/mastermind/CommandLineInterfaceTest.java
+++ test/com/mastermind/CommandLineInterfaceTest.java
@@ -17,4 +17,12 @@ public class CommandLineInterfaceTest {
         List<Color> expected = Arrays.asList(Color.yellow, Color.red, Color.blue, Color.green);
         assertEquals(expected, code);
     }
+
+    @Test
+    public void convertUserGuessToAllLowerCase() {
+        String userGuess = "GBRY";
+        List<Color> code = cli.convertInput(userGuess);
+        List<Color> expected = Arrays.asList(Color.green, Color.blue, Color.red, Color.yellow);
+        assertEquals(expected, code);
+    }
 }
