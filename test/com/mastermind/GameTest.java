776b331102fec6a7f0235bc76699c2950d04895c
Meagan Waller <meaganewaller@gmail.com>
2013-11-19 16:37:20 -0500

    Starts unit testing the game class

--- test/com/mastermind/GameTest.java
+++ test/com/mastermind/GameTest.java
@@ -1,4 +1,20 @@
 package com.mastermind;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
 public class GameTest {
+
+    @Test
+    public void returnsFalseWhenNotSolved() {
+        Game game = new Game();
+        List<Turn> turns = new ArrayList<Turn>();
+        assertTrue(game.notSolved(turns, 4));
+    }
+
+
 }
