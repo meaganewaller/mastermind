e3a07015d432b158392de59c5e218dcf8750375a
Meagan Waller <meaganewaller@gmail.com>
2013-11-27 11:48:50 -0500

    AI implements decoder

--- src/com/mastermind/AI.java
+++ src/com/mastermind/AI.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class AI implements Encoder {
+public class AI implements Encoder, Decoder{
 
     @Override
     public List<Color> chooseSecretCode(int size) {
@@ -16,8 +16,48 @@ public class AI implements Encoder {
         return secretCode;
     }
 
+    @Override
+    public List<Color> promptGuess(List<Turn> turns, int size) {
+        if (turns.size() == 0) {
+            List<Color> initialGuess = Arrays.asList(
+                    Color.green, Color.green, Color.yellow, Color.yellow, Color.red, Color. red,
+                    Color.purple, Color.purple, Color.blue, Color.blue, Color.orange, Color.orange);
+            return initialGuess.subList(0, size);
+        } else {
+            List<List<Color>> possible = remainingCodes(turns, size);
+            if (possible.size() > 0 ) {
+                return possible.get(0);
+            }
+            return null;
+        }
+    }
 
-    public List<List<Color>> theCodes(int size) {
+    // filters to the remaining possible secret code combos
+    public List<List<Color>> remainingCodes(List<Turn> turns, int size) {
+        List<List<Color>> possible = allCodes(size);
+        for (Turn turn: turns) {
+            possible = filterByTurn(possible, turn);
+        }
+        return possible;
+    }
+
+    public List<List<Color>> filterByTurn(List<List<Color>> possible, Turn turn) {
+        List<List<Color>> sameFeedback = new ArrayList<List<Color>>();
+        for (List<Color> code : possible) {
+            if (producedSameFeedback(turn, code)) {
+                sameFeedback.add(code);
+            }
+        }
+        return sameFeedback;
+    }
+
+
+    // given feedback returns t/f if another code would've produced same feedback
+    public boolean producedSameFeedback(Turn turn, List<Color> code) {
+        return Board.getFeedback(turn.getGuess(), code).equals(turn.getFeedback());
+    }
+        // produces a sequence of all possible codes for a given size
+    public List<List<Color>> allCodes(int size) {
         Color[] colors = Color.values();
         List<List<Color>> codes = new ArrayList<List<Color>>();
         if (size == 1) {
@@ -25,17 +65,15 @@ public class AI implements Encoder {
                 codes.add(Arrays.asList(color));
             }
         } else {
-            List<List<Color>> smallerCodes = theCodes(size - 1);
+            List<List<Color>> smallerCodes = allCodes(size - 1);
             for (Color color : colors) {
-                for (List<Color> smallCode : smallerCodes) {
+                for (List<Color> smallCodes : smallerCodes) {
                     List<Color> code = new ArrayList<Color>(Arrays.asList(color));
-                    code.addAll(smallCode);
+                    code.addAll(smallCodes);
                     codes.add(code);
                 }
             }
         }
-
         return codes;
     }
-
 }
--- test/com/mastermind/AITest.java
+++ test/com/mastermind/AITest.java
@@ -2,24 +2,80 @@ package com.mastermind;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotSame;
 
 public class AITest {
+    private AI ai = new AI();
+
     @Test
     public void generatesACodeWithSameSizePassedIn() {
-        AI ai = new AI();
         List<Color> secretCode = ai.chooseSecretCode(4);
         assertEquals(4, secretCode.size());
     }
 
-    @Test
+    @Test // this fails sometimes because idk how to test randomness.
     public void generatesARandomCode() {
-        AI ai = new AI();
         List<Color> secretCode =  ai.chooseSecretCode(4);
         assertNotSame(secretCode.get(0), secretCode.get(1));
+    }
 
+    @Test
+    public void allCodesWithSizeOne() {
+        List<List<Color>> oneSecretCode = ai.allCodes(1);
+        assertEquals(6, oneSecretCode.size()); // 6 to the power of 1
+        for (List<Color> code : oneSecretCode) {
+            assertEquals(1, code.size());
+        }
+        assertEquals(Color.yellow, oneSecretCode.get(0).get(0));
+        assertEquals(Color.green, oneSecretCode.get(5).get(0));
+    }
+
+    @Test
+    public void allCodesWorksWithSizeTwo() {
+        List<List<Color>> twoSecretCode = ai.allCodes(2);
+        assertEquals(36, twoSecretCode.size()); // 6 to the power of 2
+        for (List<Color> code : twoSecretCode) {
+            assertEquals(2, code.size());
+        }
+        assertEquals(Arrays.asList(Color.yellow, Color.yellow), twoSecretCode.get(0));
+        assertEquals(Arrays.asList(Color.yellow, Color.red), twoSecretCode.get(1));
+        assertEquals(Arrays.asList(Color.green, Color.green), twoSecretCode.get(35));
+    }
+
+    @Test
+    public void allCodesWorksWithSizeFour() {
+        List<List<Color>> fourSecretCode = ai.allCodes(4);
+        assertEquals(1296, fourSecretCode.size()); // 6 to the power of 4
+        for (List<Color> code : fourSecretCode) {
+            assertEquals(4, code.size());
+        }
+        assertEquals(Arrays.asList(Color.yellow, Color.yellow, Color.yellow, Color.yellow), fourSecretCode.get(0));
+        assertEquals(Arrays.asList(Color.yellow, Color.yellow, Color.yellow, Color.red), fourSecretCode.get(1));
+        assertEquals(Arrays.asList(Color.green, Color.green, Color.green, Color.green), fourSecretCode.get(1295));
+    }
+
+    @Test
+    public void makesAnInitialGuess() {
+        List<Turn> emptyBoard = new ArrayList<Turn>();
+        List<Color> initialGuess = ai.promptGuess(emptyBoard, 4);
+        List<Color> expected = Arrays.asList(Color.green, Color.green, Color.yellow, Color.yellow);
+        assertEquals(expected, initialGuess);
+    }
+
+    @Test
+    public void makesAnotherGuess() {
+        List<Color> guess = Arrays.asList(Color.green, Color.yellow, Color.blue, Color.red);
+        Feedback feedback = new Feedback(2, 0);
+        List<Turn> board = new ArrayList<Turn>();
+        Turn turn = new Turn(guess, feedback);
+        board.add(turn);
+        List<Color> secondGuess = ai.promptGuess(board, 4);
+        List<Color> expected = Arrays.asList(Color.yellow, Color.yellow, Color.yellow, Color.red);
+        assertEquals(expected, secondGuess);
     }
 }
